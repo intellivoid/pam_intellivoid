a418d2999b36b6a51e9042739818794d150eeb5c
Justin Crawford <Justasic@Gmail.com>
2017-10-30 22:35:38 -0700

    Created a system information module for Linux PAM
    This module collects some basic system information and statistical
    analytics for display upon login. It is useful for knowing what server
    you're on when SSH'ing into a box.
    
    This module is incomplete and very buggy right now, I wouldn't trust it
    on a production machine. It requires C++11 and another project called
    embedFiglet (found here: https://github.com/ebertolazzi/embedFiglet) to
    compile. embedFiglet must be compiled with -fPIC to link against the PAM
    module.

--- buildPam.sh
+++ buildPam.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-gcc -fPIC -fno-stack-protector -c src/mypam.c
+clang++ -shared -o pam_sysinfo.so -std=c++17 -fPIC -rdynamic -fno-stack-protector src/pam_sysinfo.cpp -lpam -ldl -lembedFiglet
 
-sudo ld -x --shared -o /lib/security/mypam.so mypam.o
+sudo install pam_sysinfo.so /lib/security/pam_sysinfo.so
+#sudo ld -x --shared -o /lib/security/pam_sysinfo.so pam_sysinfo.o
 
-rm mypam.o
+#rm mypam.o
